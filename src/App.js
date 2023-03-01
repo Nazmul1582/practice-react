77703916fd3bd86f70b050f1d7b5356a8061c3d0
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-03-01 11:01:21 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,110 +1,23 @@
-// import ParentComp from './component/class4/ParentComp';
-// import ChildComp from './component/class4/ChildComp';
-// import ConditionalRendering from './component/class4/ConditionalRendering';
-// import PrevState from './component/class5/PrevState';
-// import ListRender from './component/class5/ListRender';
-// import ShowInfo from './component/class5/secondWay/ShowInfo';
-// import Button from './component/class6/CssComponent/CssModules/Button';
-// import CssStyleComp from './component/class6/CssComponent/CssStyleComp';
-// import InlineCssComp from './component/class6/CssComponent/InlineCssComp';
-// import ControlledFrom from './component/class7/Form/ControlledFrom';
-// import UncontrolledFrom from './component/class7/Form/UncontrolledFrom';
-// import MountingComp from './component/class8/Lifecycle/MountingComp';
-// import UpdatingComp from './component/class8/Lifecycle/UpdatingComp';
-// import UnmountingComp from './component/class8/Lifecycle/UnmountingComp';
-// import FragmentComp from './component/class9/FragmentComp';
-// import TableComp from './component/class9/TableComp';
-// import MainComp from './component/class10/PureComp/MainComp';
-// import InputRefsComp from './component/class11/Ref/InputRefsComp';
-// import RefsComp from './component/class11/Ref/RefsComp';
-// import ParentComp from './component/class11/ErrorBoundary/ParentComp';
-// import Onclick from './component/class12/HOC/Onclick';
-// import MouseOver from './component/class12/HOC/MouseOver';
-// // import ProductComp from './component/class13/ProductComp';
-// import ClickCounter from './component/class13/RenderProps/ClickCounter';
-// import Counting from './component/class13/RenderProps/Counting';
-// import HoverCounter from './component/class13/RenderProps/HoverCounter';
-// import ParentComp from './component/class14/ParentComp';
-// import Admit from './component/class15/HttpRequest/Admit';
-// import AllPost from './component/class15/HttpRequest/AllPost';
-// import CreatePost from './component/class15/HttpRequest/CreatePost';
-// import GetPerson from './component/class15/HttpRequest/GetPerson';
-// import GetPosts from './component/class15/HttpRequest/GetPosts';
-// import UseStateComp from './component/Hooks/useState/UseStateComp';
-// import UseStateWithPrevState from './component/Hooks/useState/UseStateWithPrevState';
-// import UseStateWithObjArr from './component/Hooks/useState/UseStateWithObjArr';
-// import UseEffectComp from './component/Hooks/useEffect/UseEffectComp';
-// import FetchDataWithUseEffect from './component/Hooks/useEffect/FetchDataWithUseEffect';
-// import FetchSingleData from './component/task/FetchSingleData';
-import ParentComponent from './component/Hooks/useContext/context/ParentComponent';
-
-// import StudentComp from "./component/StudentComp";
-// import MyClassComp from './component/MyClassComp';
-// import Table from './component/task/Table'
-// import Task from './component/Task';
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div style={{textAlign: "center"}}>
-      {/* <StudentComp name = "Bayezid" age = "10" /> */}
-      {/* <MyClassComp name = "Nazmul" age = {23} />
-      <MyClassComp name = "Hasan" age = {18} /> */}
-      {/* <MyClassComp />  */}
-      {/* <ParentComp>
-        <ChildComp />
-      </ParentComp> */}
-      {/* <ParentComp /> */}
-      {/* <ConditionalRendering /> */}
-      {/* <PrevState /> */}
-      {/* <ListRender /> */}
-      {/* <ShowInfo /> */}
-      {/* <Table /> */}
-      {/* <Task /> */}
-
-      {/* <CssStyleComp />
-      <InlineCssComp />
-      <Button /> */}
-
-      {/* <ControlledFrom /> */}
-      {/* <UncontrolledFrom /> */}
-
-      {/* <MountingComp /> */}
-      {/* <UpdatingComp /> */}
-      {/* <UnmountingComp /> */}
-
-      {/* <FragmentComp /> */}
-      {/* <TableComp /> */}
-
-      {/* <MainComp /> */}
-
-      {/* <InputRefsComp /> */}
-      {/* <RefsComp /> */}
-      {/* <ParentComp /> */}
-
-      {/* <Onclick age="23" />
-      <MouseOver color='Green' /> */}
-
-      {/* <ProductComp name={(isAvailable) => isAvailable ? ("Laptop") : 'Not available'} /> */}
-      {/* <Counting render={(count, handler) => <ClickCounter count={count} handler={handler} />} />
-      <Counting render={(stateValue, method) => <HoverCounter count={stateValue} handler={method} />} /> */}
-
-      {/* <ParentComp /> */}
-
-      {/* <AllPost /> */}
-      {/* <GetPosts /> */}
-      {/* <CreatePost /> */}
-      {/* <Admit /> */}
-      {/* <GetPerson /> */}
-
-      {/* <UseStateComp />
-      <UseStateWithPrevState /> */}
-      {/* <UseStateWithObjArr /> */}
-
-      {/* <UseEffectComp /> */}
-      {/* <FetchDataWithUseEffect /> */}
-      {/* <FetchSingleData /> */}
-      <ParentComponent />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- src/component/Hooks/useContext/context/ProductContext.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Product = React.createContext();
-const ProductProvider = Product.Provider;
-
-export { ProductProvider };
-export default Product
